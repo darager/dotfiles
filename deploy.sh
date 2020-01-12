e15611444998ea46a0de0092dbdab84335d1e6a9
Rager Florian <florianrager90@gmail.com>
2020-01-12 15:59:48 +0100

    removes unecessary downloads from setup script

--- deploy.sh
+++ deploy.sh
@@ -10,25 +10,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # install custom font for powerline
 sudo apt-get install fonts-powerline
 
-# install ctags
-sudo apt-get install exuberant-ctags
-
-# install youcompleteme
-sudo apt-get install build-essential cmake python-dev python3-dev
-mkdir ~/.vim/plugged/ && cd ~/.vim/plugged/
-git clone https://github.com/ycm-core/YouCompleteMe
-cd YouCompleteMe
-git submodule update --init --recursive
-./install.py --all
-
-# install komodo-remote-debugging-package for python remote debugging
-cd ~/.vim
-mkdir vimhelpers && cd vimhelpers
-curl http://downloads.activestate.com/Komodo/releases/11.1.0/remotedebugging/Komodo-PythonRemoteDebugging-11.1.0-91033-linux-x86.tar.gz --output komodo-remote.tar.gz
-tar -xzvf komodo-remote.tar.gz
-rm komodo-remote.tar.gz
-# in order for komodo to work the dbgp folder has to be in the folder that the binary is in
-mv ~/.vim/vimhelpers/Komodo-PythonRemoteDebugging-11.1.0-91033-linux-x86/python3lib/dbgp ~/.vim/vimhelpers/Komodo-PythonRemoteDebugging-11.1.0-91033-linux-x86/
-
 clear
 echo "In order to install the plugins open vim and run $(tput bold)$(tput setaf 0):PlugInstall"
