d0538fb58fd26dd645f3c5aa8f8e5e217fe420b9
Rager <florianrager90@gmail.com>
2020-02-09 18:29:44 +0100

    repairs setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir ~/.config/nvim
+sudo apt-get install neovim
 
-ln ./.bashrc ~/
-ln ./.tmux.conf ~/
-ln ./init.vim ~/.config/nvim
+ln ./.bashrc ~/.bashrc
+ln ./.tmux.conf ~/.tmux.conf
+
+mkdir ~/.config/nvim
+ln ./init.vim ~/.config/nvim/init.vim
 
 # install plug.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# install neovim
-sudo apt-get install neovim
 
 clear
 echo "In order to install the plugins open vim and run $(tput bold)$(tput setaf 0):PlugInstall"
