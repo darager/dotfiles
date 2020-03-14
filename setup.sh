4ce8d2f90c7b5ab492deb898d9f452e6555d6108
Florian Rager <florianrager90@gmail.com>
2020-03-14 19:56:22 +0100

    updates setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get upgrade
+
+# install latest version of neovim (required by coc-vim)
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get install neovim
 
-ln ./.bashrc ~/.bashrc
-ln ./.tmux.conf ~/.tmux.conf
-
-mkdir ~/.config/nvim
-ln ./init.vim ~/.config/nvim/init.vim
-
 # install plug.vim
+mkdir ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install nodejs
+sudo apt-get install nodejs
+
+if [ ! -f ~/.config/init.vim ]; then
+    touch ~/.config/init.vim
+fi
+
+if [ ! -f ~/.bashrc ]; then
+    touch ~/.bashrc
+fi
+
+if [ ! -f ~/.tmux.conf ]; then
+    touch ~/.tmux.conf
+fi
+
+echo "source ~/dotfiles/.bashrc" > ~/.bashrc
+echo "source ~/dotfiles/.tmux.conf" > ~/.tmux.conf
+echo "source ~/dotfiles/init.vim" > ~/.config/nvim/init.vim
+
 
 clear
 echo "In order to install the plugins open vim and run $(tput bold)$(tput setaf 0):PlugInstall"
